32dc492f2c07e8fd26ed46aa2573c4c9dc57b41c
Frank Zhu <frankzhu2003@gmail.com>
2023-03-01 10:55:17 -0500

    Update setupDemoEnv.sh

--- setupDemoEnv.sh
+++ setupDemoEnv.sh
@@ -3,7 +3,13 @@
 #export CHRONOSPHERE_ORG_NAME=rc-collec
 #export CHRONOSPHERE_API_TOKEN=37d84637cbd6d23
 
+gcloud auth login --update-adc
+droidcli auth assume-role -r cluster-admin -c prod-test 
+
 eval $(~/chrono/github/monorepo/scripts/set-chrono-env $1)
 
 chronoctl preview service-accounts list
 alias c=chronoctl
+
+echo "CHRONOSPHERE_ORG_NAME=$CHRONOSPHERE_ORG_NAME"
+echo "CHRONOSPHERE_API_TOKEN=$CHRONOSPHERE_API_TOKEN"
